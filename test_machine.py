d9ae58c5ce43e9b52b4d87f39b27cb5beba885fa
Hansika Bandela <hansikabandela@gmail.com>
2026-02-02 20:05:04 -0800

    renamed test function for pytest

--- test_machine.py
+++ test_machine.py
@@ -1,20 +1,20 @@
 
 from platform_model import Machine
 
-def machine_begins():
+def test_machine_begins():
     machine = Machine()
     res = machine.start()
     assert res == "System has started"
     assert machine.condition == "ACTIVE"
 
-def machine_invalid_command():
+def test_machine_invalid_command():
     machine = Machine()
     machine.start()
     res = machine.direct_command("INVALID")
     assert res == "Inaccurate command passed"
     assert machine.condition == "ERROR"
 
-def machine_reboot():
+def test_machine_reboot():
     machine = Machine()
     machine.start()
     machine.direct_command("RESTART")
